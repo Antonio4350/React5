02ce97f8bda15063a8a568805744e33244c263e7
Antonio4350 <antoniocruzvalletto00@gmail.com>
2025-09-17 01:18:25 -0300

    arreglo estilo

--- react/src/components/PantallaJuegos.jsx
+++ react/src/components/PantallaJuegos.jsx
@@ -7,12 +7,12 @@ export default function PantallaJuegos() {
     <div className="relative min-h-screen flex flex-col items-center justify-center">
       <Fondo />
 
-      <div className="relative z-10 bg-black/80 border-4 border-white p-10 rounded-lg shadow-lg text-center max-w-3xl">
-        <h1 className="text-4xl font-bold text-white mb-10 retro-text">
+      <div className="relative z-10 bg-black/80 border-4 border-white p-6 rounded-lg shadow-lg text-center max-w-3xl">
+        <h1 className="text-3xl font-bold text-white mb-6 retro-text">
           SELECCIONA UN JUEGO
         </h1>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
           <Link
             to="/space"
             className="group flex flex-col items-center border-2 border-white p-4 rounded-lg bg-black hover:bg-white transition"
@@ -20,9 +20,9 @@ export default function PantallaJuegos() {
             <img
               src="./fondoSpace.png"
               alt="Space Game"
-              className="w-90 h-90 object-contain mb-2 group-hover:scale-110 transition"
+              className="w-60 h-60 object-contain mb-2 group-hover:scale-110 transition"
             />
-            <span className="text-white group-hover:text-black retro-text">
+            <span className="text-white group-hover:text-black retro-text text-sm md:text-base text-center">
               SPACE INVADERS REMAKE
             </span>
           </Link>
@@ -34,9 +34,9 @@ export default function PantallaJuegos() {
             <img
               src="./1942fondo.png"
               alt="Guerra Game"
-              className="w-90 h-90 object-contain mb-2 group-hover:scale-110 transition"
+              className="w-60 h-60 object-contain mb-2 group-hover:scale-110 transition"
             />
-            <span className="text-white group-hover:text-black retro-text">
+            <span className="text-white group-hover:text-black retro-text text-sm md:text-base text-center">
               1942 REMAKE
             </span>
           </Link>
